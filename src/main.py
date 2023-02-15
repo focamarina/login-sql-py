a612c57f98a5d5d7d2fd418bde9b36b4b27e142a
Foca Marina <alvaroriba585@gmail.com>
2023-02-15 17:06:53 -0300

    v2
    Registro con subida a bd

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
 from getpass import getpass
+import mysql.connector
 import time
 
+conexion = mysql.connector.connect(
+    host='localhost', user='', passwd='', database='')
+cursor = conexion.cursor()
+
+
+print("""
+ .----------------.  .----------------.  .----------------.  .----------------.     .----------------.  .----------------.  .----------------.  .----------------. 
+| .--------------. || .--------------. || .--------------. || .--------------. |   | .--------------. || .--------------. || .--------------. || .--------------. |
+| |   ______     | || |     ____     | || |   _____      | || | _____  _____ | |   | | _____  _____ | || |  _________   | || |    _______   | || |  _________   | |
+| |  |_   _ \    | || |   .'    `.   | || |  |_   _|     | || ||_   _||_   _|| |   | ||_   _||_   _|| || | |_   ___  |  | || |   /  ___  |  | || | |  _   _  |  | |
+| |    | |_) |   | || |  /  .--.  \  | || |    | |       | || |  | |    | |  | |   | |  | | /\ | |  | || |   | |_  \_|  | || |  |  (__ \_|  | || | |_/ | | \_|  | |
+| |    |  __'.   | || |  | |    | |  | || |    | |   _   | || |  | '    ' |  | |   | |  | |/  \| |  | || |   |  _|  _   | || |   '.___`-.   | || |     | |      | |
+| |   _| |__) |  | || |  \  `--'  /  | || |   _| |__/ |  | || |   \ `--' /   | |   | |  |   /\   |  | || |  _| |___/ |  | || |  |`\____) |  | || |    _| |_     | |
+| |  |_______/   | || |   `.____.'   | || |  |________|  | || |    `.__.'    | |   | |  |__/  \__|  | || | |_________|  | || |  |_______.'  | || |   |_____|    | |
+| |              | || |              | || |              | || |              | |   | |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' |   | '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------'     '----------------'  '----------------'  '----------------'  '----------------' 
+""")
+time.sleep(3)
 
 print("""
         Opciones para el usuario:
@@ -19,12 +39,10 @@ elif option.isdigit() == True:
     option = int(option)
 
     if option == 1:
-        username = input("Ingrese el nombre de usuario: ")
-        pwd = getpass(prompt='Ingrese su contraseña: ')
-        print('Iniciando sesion...')
-        time.sleep(2)
-        print(username)
-        print(pwd)
+        # username = input("Ingrese el nombre de usuario: ")
+        # pwd = getpass(prompt='Ingrese su contraseña: ')
+        # print('Iniciando sesion...')
+        print('PROXIMAMENTE...')
 
     elif option == 2:
         name = input('¿Cual es su nombre?: ')
@@ -38,6 +56,12 @@ elif option.isdigit() == True:
             'Ingrese su pais, provincia y localidad (Hacerlo seguidos de coma: España, Madrid, Madrid): ')
         pwd = getpass('Escriba una contraseña: ')
         print('\nGracias por registrarse! Bienvenido '+name+'!')
+
+        sql = "INSERT INTO `users`(`name`, `surname`, `email`, `username`, `contraseña`, `phnmro`, `birthday`, `c/p/l`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+        datos = (name, surname, email, username, pwd, phone, brth, cpl)
+        cursor.execute(sql, datos)
+        conexion.commit()
+        conexion.close()
 
     else:
         print('Numero incorrecto!')
